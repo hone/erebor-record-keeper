c683f55af210f43f195990c0c508a88564b1a32c
Terence Lee <hone02@gmail.com>
2020-08-04 11:36:06 -0500

    move to standard framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use serenity::{
+    client::Client,
+    framework::standard::{
+        macros::{command, group},
+        CommandResult, StandardFramework,
+    },
+    model::{channel::Message, gateway::Ready},
+    prelude::{Context, EventHandler},
+};
+
+struct Handler;
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[group]
+#[commands(ping)]
+struct General;
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Pong!")?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    #[cfg(debug_assertions)]
+    dotenv::dotenv().ok();
+
+    let discord_token =
+        std::env::var("DISCORD_TOKEN").expect("Please provide the env var DISCORD_TOKEN");
+
+    let mut client = Client::new(discord_token, Handler).expect("Error creating client");
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("!"))
+            .group(&GENERAL_GROUP),
+    );
+
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 }
