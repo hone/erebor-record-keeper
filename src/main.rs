bdd11a7f2ebd5084157924e5c27a88d7ad127676
Terence Lee <hone02@gmail.com>
2020-08-17 23:23:56 -0500

    use database to fetch quests

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use erebor_record_keeper::hob_scenario_parser::{fetch, Scenario};
-use rand::{rngs::SmallRng, Rng, SeedableRng};
 use serenity::{
     async_trait,
     client::Client,
     framework::standard::{
         macros::{command, group},
-        CommandResult, StandardFramework,
+        Args, CommandResult, StandardFramework,
     },
     model::{channel::Message, gateway::Ready},
     prelude::{Context, EventHandler, TypeMapKey},
 };
+use sqlx::postgres::PgPoolOptions;
+
+const DEFAULT_QUESTS_NUM: i64 = 3;
+
+struct PostgresPool;
+impl TypeMapKey for PostgresPool {
+    type Value = sqlx::postgres::PgPool;
+}
 
 struct QuestData;
 impl TypeMapKey for QuestData {
@@ -36,20 +43,26 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
-async fn quest(ctx: &Context, msg: &Message) -> CommandResult {
+async fn quest(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let quantity = args.single::<i64>().unwrap_or(DEFAULT_QUESTS_NUM);
     let data = ctx.data.read().await;
-    let scenarios = data
-        .get::<QuestData>()
-        .expect("Expected QuestData in TypeMap.");
-    let indexes = (0..3).fold(Vec::with_capacity(3), |mut indexes, _| {
-        let index = random_index(&indexes, scenarios.len());
-        indexes.push(index);
+    let pool = data
+        .get::<PostgresPool>()
+        .expect("Expected PgPoolOptions in TypeMap.");
+    let scenarios = sqlx::query!(
+        r#"
+SELECT scenarios.title, sets.name AS set
+FROM scenarios, sets
+WHERE scenarios.set_id = sets.id
+ORDER BY RANDOM()
+LIMIT $1;
+"#,
+        quantity
+    )
+    .fetch_all(pool)
+    .await?;
 
-        indexes
-    });
-    for (i, &index) in indexes.iter().enumerate() {
-        let scenario = &scenarios[index];
-
+    for (i, scenario) in scenarios.iter().enumerate() {
         msg.channel_id
             .say(
                 &ctx.http,
@@ -66,17 +79,6 @@ async fn quest(ctx: &Context, msg: &Message) -> CommandResult {
     Ok(())
 }
 
-fn random_index(existing: &[usize], max: usize) -> usize {
-    let mut rng = SmallRng::from_entropy();
-    let mut index = max;
-
-    while index >= max || existing.contains(&index) {
-        index = rng.gen_range(0, max);
-    }
-
-    index
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(debug_assertions)]
@@ -84,6 +86,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let discord_token =
         std::env::var("DISCORD_TOKEN").expect("Please provide the env var DISCORD_TOKEN");
+    let database_url =
+        std::env::var("DATABASE_URL").expect("Please provide the env var DATABASE_URL");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
 
     let mut client = Client::new(discord_token)
         .event_handler(Handler)
@@ -97,6 +106,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let scenarios = fetch().await.unwrap();
         println!("Loading all {} scenarios", scenarios.len());
         let mut data = client.data.write().await;
+        data.insert::<PostgresPool>(pool);
         data.insert::<QuestData>(scenarios);
     }
 
