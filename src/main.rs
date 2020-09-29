9c39ad964c6186468b613cbc70a3e70f59c23891
Terence Lee <hone02@gmail.com>
2020-09-29 00:22:44 -0500

    restrict channels to join

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ use serenity::{
     async_trait,
     client::Client,
     framework::standard::{macros::group, StandardFramework},
-    model::gateway::Ready,
+    model::{gateway::Ready, id::ChannelId},
     prelude::{Context, EventHandler},
 };
 use sqlx::postgres::PgPoolOptions;
+use std::collections::HashSet;
 
 struct Handler;
 #[async_trait]
@@ -33,7 +34,7 @@ struct General;
 struct Event;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     #[cfg(debug_assertions)]
     dotenv::dotenv().ok();
 
@@ -41,6 +42,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         std::env::var("DISCORD_TOKEN").expect("Please provide the env var DISCORD_TOKEN");
     let database_url =
         std::env::var("DATABASE_URL").expect("Please provide the env var DATABASE_URL");
+    let channels = parse_channels(&std::env::var("DISCORD_CHANNELS").unwrap_or("".to_string()))
+        .expect("Please use comma separated list of Channel Ids in the env var DISCORD_CHANNELS");
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -51,7 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .event_handler(Handler)
         .framework(
             StandardFramework::new()
-                .configure(|c| c.prefix("!"))
+                .configure(|c| c.prefix("!").allowed_channels(channels))
                 .group(&EVENT_GROUP)
                 .group(&GENERAL_GROUP),
         )
@@ -67,4 +70,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+fn parse_channels(input: &str) -> anyhow::Result<HashSet<ChannelId>> {
+    input
+        .split(",")
+        .map(|str| Ok(ChannelId(str.parse::<u64>()?)))
+        .collect::<anyhow::Result<HashSet<ChannelId>>>()
 }
