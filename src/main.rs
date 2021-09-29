5d8a71d90b660fd14c24bcacc527b0372ba2d5a8
Terence Lee <hone02@gmail.com>
2021-09-29 02:01:53 -0500

    stop restricting the bot to specific chananels

--- src/main.rs
+++ src/main.rs
@@ -81,8 +81,6 @@ async fn main() {
         std::env::var("DISCORD_TOKEN").expect("Please provide the env var DISCORD_TOKEN");
     let database_url =
         std::env::var("DATABASE_URL").expect("Please provide the env var DATABASE_URL");
-    let channels = parse_channels(&std::env::var("DISCORD_CHANNELS").unwrap_or("".to_string()))
-        .expect("Please use comma separated list of Channel Ids in the env var DISCORD_CHANNELS");
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -94,7 +92,7 @@ async fn main() {
         .event_handler(Handler)
         .framework(
             StandardFramework::new()
-                .configure(|c| c.prefix("!").allowed_channels(channels))
+                .configure(|c| c.prefix("!"))
                 .help(&MY_HELP)
                 .group(&EVENT_GROUP)
                 .group(&GENERAL_GROUP),
@@ -110,11 +108,4 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
-}
-
-fn parse_channels(input: &str) -> anyhow::Result<HashSet<ChannelId>> {
-    input
-        .split(",")
-        .map(|str| Ok(ChannelId(str.parse::<u64>()?)))
-        .collect::<anyhow::Result<HashSet<ChannelId>>>()
 }
